c14a93b34d3218211ed86902e436e7e865303288
Ninad More <ninadyogeshmore@gmail.com>
2023-09-16 20:38:16 +0530

    Minor

--- lib/presentation/journeys/search_movie/search_movie_card.dart
+++ lib/presentation/journeys/search_movie/search_movie_card.dart
@@ -7,69 +7,6 @@ import '../../themes/text_theme.dart';
 import '../movie_detail/movie_detail_arguments.dart';
 import '../movie_detail/movie_detail_screen.dart';
 
-// class SearchMovieCard extends StatelessWidget {
-//   final MovieEntity movie;
-
-//   const SearchMovieCard({
-//     super.key,
-//     required this.movie,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap: () {
-//         Navigator.of(context).push(
-//           MaterialPageRoute(
-//             builder: (context) => MovieDetailScreen(
-//               movieDetailArguments: MovieDetailArguments(movie.id),
-//             ),
-//           ),
-//         );
-//       },
-//       child: Padding(
-//         padding: const EdgeInsets.symmetric(
-//           horizontal: 16,
-//           vertical: 8,
-//         ),
-//         child: Row(
-//           children: [
-//             Padding(
-//               padding: const EdgeInsets.all(8),
-//               child: ClipRRect(
-//                 borderRadius: BorderRadius.circular(8),
-//                 child: CachedNetworkImage(
-//                   imageUrl: '${ApiConstants.BASE_IMG_URL}${movie.posterPath}',
-//                   width: 100,
-//                 ),
-//               ),
-//             ),
-//             Expanded(
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 mainAxisSize: MainAxisSize.max,
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: [
-//                   Text(
-//                     movie.title,
-//                     style: Theme.of(context).textTheme.titleMedium,
-//                   ),
-//                   Text(
-//                     movie.overview,
-//                     maxLines: 3,
-//                     overflow: TextOverflow.ellipsis,
-//                     style: Theme.of(context).textTheme.greyCaption,
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class SearchMovieCard extends StatelessWidget {
   final MovieEntity movie;
 
@@ -92,18 +29,18 @@ class SearchMovieCard extends StatelessWidget {
       },
       child: Padding(
         padding: const EdgeInsets.symmetric(
-          horizontal: 10,
-          vertical: 10,
+          horizontal: 16,
+          vertical: 8,
         ),
         child: Row(
           children: [
             Padding(
-              padding: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(8),
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(8),
                 child: CachedNetworkImage(
                   imageUrl: '${ApiConstants.BASE_IMG_URL}${movie.posterPath}',
-                  width: 40,
+                  width: 100,
                 ),
               ),
             ),
